5ca67cab30e46f3564a424dd319639786d02ed00
Dmitriy Hlebay <webb17@yandex.ru>
2020-02-22 13:39:58 +0300

    fix: correct task

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ module.exports = function toReadable (number) {
       let arr = String(number).split('');
   
       
-      arr.length == 1 ? array.push(TwentyNumbers[+arr[0]]):
-      arr.length == 2 && arr[0] == 1 ? array.push(TwentyNumbers[number]):
-      arr.length == 2 ? array.push(decades[+arr[0]],TwentyNumbers[+arr[1]]):
-      arr.length == 3 && arr[1] == 0 && arr[2] == 0 ? array.push(TwentyNumbers[+arr[0]],"hundred"):
-      arr.length == 3 && arr[1] == 0 ? array.push(TwentyNumbers[+arr[0]],"hundred",TwentyNumbers[+arr[0]]):
-      arr.length == 3 && arr[1] == 1 ? array.push(TwentyNumbers[+arr[0]],"hundred",TwentyNumbers[10 + +arr[2]]):
-      arr.length == 3 ? array.push(TwentyNumbers[+arr[0]],"hundred",decades[+arr[1]],TwentyNumbers[+arr[2]]):'';
-      return array.join(' ');
+    arr.length == 1 ? array.push(TwentyNumbers[+arr[0]]):
+    arr.length == 2 && arr[0] == 1 ? array.push(TwentyNumbers[number]):
+    arr.length == 2 && arr[1] == 0 ? array.push(decades[+arr[0]]):
+    arr.length == 2 ? array.push(decades[+arr[0]],TwentyNumbers[+arr[1]]):
+    arr.length == 3 && arr[1] == 0 && arr[2] == 0 ? array.push(TwentyNumbers[+arr[0]],"hundred"):
+    arr.length == 3 && arr[1] == 0 ? array.push(TwentyNumbers[+arr[0]],"hundred",TwentyNumbers[+arr[2]]):
+    arr.length == 3 && arr[1] == 1 ? array.push(TwentyNumbers[+arr[0]],"hundred",TwentyNumbers[10 + +arr[2]]):
+    arr.length == 3 && arr[2] == 0 ? array.push(TwentyNumbers[+arr[0]],"hundred",decades[+arr[1]]):
+    arr.length == 3 ? array.push(TwentyNumbers[+arr[0]],"hundred",decades[+arr[1]],TwentyNumbers[+arr[2]]):'';
+    return array.join(' ');
 }
